e8e17c5657e991c1c8a87b415e55d9811fae3814
Sarah Arnold <digipet007@gmail.com>
2019-10-16 18:13:03 -0700

    added process.env

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: process.env.PORT || 3306,
     user: "root",
     password: "flowerfa1!",
     database: "bamazon"
